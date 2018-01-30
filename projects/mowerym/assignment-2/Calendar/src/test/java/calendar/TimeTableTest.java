7f59ecb5c3a027ffa3207c587f94d97dd0bde56a
mowerym <mowerym@oregonstate.edu>
2018-01-29 20:13:28 -0800

    final adjustments
    These are the final changes to my assignment. Completed the
    TimeTableTest class.

--- projects/mowerym/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
+++ projects/mowerym/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 import org.junit.Test;
-import sun.util.calendar.Gregorian;
+//import sun.util.calendar.Gregorian;
 
 import static org.junit.Assert.*;
 
@@ -24,9 +24,9 @@ public class TimeTableTest {
 	 	LinkedList<Appt> lists = new LinkedList<Appt>();
 	 	LinkedList<Appt> cpr = new LinkedList<Appt>();
 
-		 int startHour=21;
+		 int startHour=15;
 		 int startMinute=30;
-		 int startDay=15;
+		 int startDay=21;
 		 int startMonth=01;
 		 int startYear=2018;
 		 String title="Birthday Party";
@@ -40,12 +40,12 @@ public class TimeTableTest {
 				 title,
 				 description);
 
-		 int[] recurArray={1,2,3};
+		 int[] recurArray={2,3,4};
 		 appt.setRecurrence( recurArray, Appt.RECUR_BY_WEEKLY, 2, Appt.RECUR_NUMBER_FOREVER);
 	 	 lists.add(appt);
 
 	 	 TimeTable timeTable = new TimeTable();
-		 GregorianCalendar firstDay = new GregorianCalendar(2016,10,15);
+		 GregorianCalendar firstDay = new GregorianCalendar(2018,01,21);
 		 GregorianCalendar lastDay = (GregorianCalendar)firstDay.clone();
 		 lastDay.add(Calendar.DAY_OF_MONTH,10);
 		 LinkedList<CalDay> calDays = new LinkedList<CalDay>();
@@ -56,15 +56,72 @@ public class TimeTableTest {
 		 cpr = calDays.get(0).getAppts();
 
 		 //assertions
-		 assertEquals(compare, calDays.get(0));
+		 //assertEquals("", calDays.get(0));
 		 assertEquals(lists, calDays.get(0).getAppts());
 		 assertEquals(appt.getRecurBy(), cpr.get(0).getRecurBy());
 
 	 }
 
-	 @Test
+	/**
+	 *
+	 * other tests including permute
+	 */
+
+	@Test
 	  public void test02()  throws Throwable  {
-		 
+		LinkedList<Appt> list1 = new LinkedList<Appt>();
+		LinkedList<Appt> list2 = new LinkedList<Appt>();
+
+		int startHour=15;
+		int startMinute=30;
+		int startDay=21;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		//Construct a new Appointment object with the initial data
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+
+		int[] recurArray={2,3,4};
+		appt.setRecurrence( recurArray, Appt.RECUR_BY_MONTHLY, 2, Appt.RECUR_NUMBER_FOREVER);
+		list1.add(appt);
+
+		startHour = 15;
+		startMinute=30;
+		startDay=22;
+		startMonth=01;
+		startYear=2018;
+		title="Second appt";
+		description="This is the second appt.";
+		//Construct a new Appointment object with the initial data
+		Appt appt2 = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		appt2.setRecurrence( recurArray, Appt.RECUR_BY_YEARLY, 2, Appt.RECUR_NUMBER_FOREVER);
+		list1.add(appt2);
+
+		int[] temp = {1,0};
+
+		TimeTable timeTable = new TimeTable();
+		GregorianCalendar firstDay = new GregorianCalendar(2018,01,21);
+		GregorianCalendar lastDay = (GregorianCalendar)firstDay.clone();
+		lastDay.add(Calendar.DAY_OF_MONTH,5);
+		LinkedList<CalDay> calDays = new LinkedList<CalDay>();
+		calDays = timeTable.getApptRange(list1, firstDay, lastDay);
+
+		list1 = timeTable.permute(list1,temp);
+
+		//assertions
+		assertEquals(list1.get(0).getStartHour(),appt.getStartHour());
 	 }
-//add more unit tests as you needed
 }
